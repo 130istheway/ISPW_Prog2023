e57f542c3437bd572332680a84fa545954888f21
Stefy <l.brigido45@gmail.com>
2024-01-23 03:51:02 +0100

    giusto

--- src/main/java/server/com/serverISPW/exception/PersonalException.java
+++
@@ -1,7 +0,0 @@
-package server.com.serverISPW.exception;
-
-public class PersonalException extends Exception {
-    public PersonalException(String errorMessage) {
-        super(errorMessage);
-    }
-}
