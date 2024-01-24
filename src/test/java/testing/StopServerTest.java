ef0f1360e8e44422345692b3b62734747990214e
Stefy <l.brigido45@gmail.com>
2024-01-24 11:45:34 +0100

    ADD test

---
+++ src/test/java/testing/StopServerTest.java
@@ -0,0 +1,45 @@
+package testing;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import server.com.server.Server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class StopServerTest {
+
+    @BeforeEach
+    void setUp(){
+        ArrayList<Thread> thread = new ArrayList<>();
+
+        Runnable server = new Server();
+        thread.add(new Thread(server));
+
+        for (Thread thread2 : thread) {
+            thread2.start();
+        }
+    }
+
+    @Test
+    void stopServer() throws IOException {
+        final String host = "localhost";
+        int port = 5000;
+
+        String response;
+
+        Socket socket = new Socket(host, port);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+        // Send a message to the server
+        out.println("STOPIT");
+        response = in.readLine();
+        System.out.println(response);
+    }
+}
