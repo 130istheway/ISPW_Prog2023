cf3d63085ad9b424054510ef932cc02c70dfd0d4
Stefy <l.brigido45@gmail.com>
2024-01-25 01:16:18 +0100

    Change
    From sonarCloud

--- src/main/java/controller/BaseController.java
+++ src/main/java/controller/BaseController.java
@@ -16,8 +16,10 @@ public class BaseController {
 
 
     private void controll(String message) throws PersonalException, IOException{
+
         switch (message) {
             case "LOGIN":
+                info.sendlog(LivelloInformazione.TRACE, "Entering LOGIN");
                 login = new LoginController(info);
                 try{
                     cred = login.execute();
@@ -31,35 +33,43 @@ public class BaseController {
                 }
                 break;
 
-           case "EXIT":
+            case "EXIT":
+                info.sendlog(LivelloInformazione.TRACE, "Entering EXIT");
                 info.sendMessage(STOPTHAT);
                 if (cred!=null) throw new PersonalException("Esco, " + cred.getUsername() + " si era autenticato ma è voluto uscire");
                 throw new PersonalException("NON si è voluto autenticare");
 
             case "WRITEBACK":
-                boolean writeBack = true;
-                info.sendMessage("WRITEBACK MODE");
-                this.info.sendlog( LivelloInformazione.TRACE ,"WRITEBACK MODE");
-                String inputLine;
-                while (((inputLine = info.getMessage()) != null) && writeBack ) {
-                    this.info.sendlog( LivelloInformazione.INFO ,"Server " + this.info.getThreadId()  + ": " + inputLine);
-                    if (inputLine.equals("STOPWRITEBACK")){
-                        info.sendMessage("WRITEBACKENDED");
-                        writeBack = false;
-                    }
-                    if (!this.info.isRunning()) {
-                        info.sendMessage(STOPTHAT);
-                        this.info.sendlog( LivelloInformazione.DEBUG ,"Server " + this.info.getThreadId()  + ": Non rispondo poichè sto chiudendo la connessione");
-                        writeBack = false;
-                    }
-                    info.sendMessage(inputLine);
-                }
-                break;
+                info.sendlog(LivelloInformazione.TRACE, "Entering WRITEBACK");
+                rispondereACioCheMandaComeUnPappagallo(info);
+                info.sendlog(LivelloInformazione.FATAL, "Sono arrivato qui : xxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+            break;
+
             default:
+                info.sendMessage("NOT A VALID COMAND");
             break;
         }
     }
 
+    private void rispondereACioCheMandaComeUnPappagallo(ControllerInfoSulThread info) throws IOException {
+        info.sendMessage("WRITEBACK MODE");
+        this.info.sendlog( LivelloInformazione.TRACE ,"WRITEBACK MODE");
+        String inputLine;
+        while ((inputLine = info.getMessage())!= null){
+            this.info.sendlog( LivelloInformazione.INFO ,"Server " + this.info.getThreadId()  + ": " + inputLine);
+            if (inputLine.equals("STOPWRITEBACK")){
+                info.sendMessage("WRITEBACKENDED");
+                info.sendlog(LivelloInformazione.FATAL, "Exiting WRITEBACK : xxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+                return;
+            }
+            if (!this.info.isRunning()) {
+                info.sendMessage(STOPTHAT);
+                this.info.sendlog( LivelloInformazione.DEBUG ,"Server " + this.info.getThreadId()  + ": Non rispondo poichè sto chiudendo la connessione");
+                return;
+            }
+            info.sendMessage(inputLine);
+        }
+    }
 
     public BaseController(ControllerInfoSulThread infoest){
         this.info = infoest;
--- src/main/java/util/MessageToCommand.java
+++ src/main/java/util/MessageToCommand.java
@@ -2,10 +2,10 @@ package util;
 
 public class MessageToCommand {
     public void fromMessage(){
-
+        throw new UnsupportedOperationException();
     }
 
     public void toMessage(){
-        
+        throw new UnsupportedOperationException();
     }
 }
