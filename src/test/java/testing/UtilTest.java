7425f7a220711d3c336f8dcee131ec57e3ff2866
Stefy <l.brigido45@gmail.com>
2024-02-08 00:44:24 +0100

    Create UtilTest.java

---
+++ src/test/java/testing/UtilTest.java
@@ -0,0 +1,43 @@
+package testing;
+
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Test;
+import util.MessageToCommand;
+import util.PayloadToCredential;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UtilTest {
+
+    @Test
+    public void testPayloadToCredential(){
+        String input = "user:abcd,pass:efgh";
+        PayloadToCredential brigido = new PayloadToCredential();
+        List<String> test = brigido.getCredentials(input);
+        List<String> inputScritto = new ArrayList<>();
+        inputScritto.add("abcd");
+        inputScritto.add("efgh");
+        assertEquals(inputScritto.get(0), test.get(0));
+        assertEquals(inputScritto.get(1), test.get(1));
+    }
+
+    @Test
+    public void testMessageToCredential(){
+        MessageToCommand message = new MessageToCommand();
+        String input = "EXIT | sei sicuro";
+        message.fromMessage(input);
+        assertEquals("sei sicuro", message.getPayload());
+        assertEquals("EXIT", message.getCommand());
+        message.setCommand("SI");
+        message.setPayload("sono veramente sicuro");
+        assertEquals("SI | sono veramente sicuro", message.toMessage());
+        message.setPayload("si|si|si");
+        assertEquals("SI | si|si|si", message.toMessage());
+        input = "INS | 1|2|3|4|5";
+        message.fromMessage(input);
+        assertEquals("1|2|3|4|5", message.getPayload());
+    }
+
+}
